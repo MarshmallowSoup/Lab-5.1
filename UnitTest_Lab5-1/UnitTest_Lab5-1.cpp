209f0cb544484964cf5ea623341343eb0b84a488
MarshmallowSoup <lysetskyyv@gmail.com>
2021-10-19 23:37:26 +0300

    Lab 5.1 with unit test

---
+++ UnitTest_Lab5-1/UnitTest_Lab5-1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "..\Lab_5-1\lab_5-1.cpp"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab51
+{
+	TEST_CLASS(UnitTestLab51)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = f(0, 0, 1);
+			Assert::AreEqual(1., t);
+		}
+	}; 
+}
