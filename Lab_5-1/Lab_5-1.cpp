9a20e73f49eae6451a2b69ef3cc3174f0aae288e
MarshmallowSoup <lysetskyyv@gmail.com>
2021-10-19 22:42:14 +0300

    first lab 5.1 commit

---
+++ Lab_5-1/Lab_5-1.cpp
@@ -0,0 +1,18 @@
+﻿// Lab 5_1
+#include <iostream>
+#include <cmath>
+using namespace std;
+double f(const double a, const double b, const double c); // прототип
+int main()
+{
+	double s, t;
+	cout << "s = "; cin >> s;
+	cout << "t = "; cin >> t;
+	double k = (f(1, t + s, s) + f(t, s * t, 1) * 1.) / (1. * (1 + pow(f(s, 1, t), 2)));
+	cout << "k = " << k << endl;
+	return 0;
+}
+double f(const double a, const double b, const double c) // визначення
+{
+	return (a * sin(b) + b * sin(a) + c * c);
+}
